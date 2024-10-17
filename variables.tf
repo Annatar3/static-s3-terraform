529dc3b75141c0222b7f6bdc9d471c2873ab2628
Annatar3 <n_demchenko@cu.edu.ge>
2024-10-17 22:56:15 +0400

    fixed unique name

--- variables.tf
+++ variables.tf
@@ -1,7 +1,7 @@
 variable "bucket_name" {
   description = "The name of the S3 bucket"
   type        = string
-  default     = "my-personal-website-bucket"
+  default     = "epam-final-aws"
 }
 
 variable "region" {
