504ea41c0b400d2d4c30a9c29ddeca44a6c44f7e
Annatar3 <n_demchenko@cu.edu.ge>
2024-10-18 02:55:57 +0400

    added cloudfront

--- main.tf
+++ main.tf
@@ -3,8 +3,8 @@ provider "aws" {
 }
 
 resource "aws_s3_bucket" "website_bucket" {
-  bucket = var.bucket_name
-  force_destroy = true 
+  bucket        = var.bucket_name
+  force_destroy = true
 }
 
 resource "aws_s3_bucket_website_configuration" "website_config" {
@@ -22,8 +22,8 @@ resource "aws_s3_bucket_website_configuration" "website_config" {
 resource "aws_s3_bucket_public_access_block" "public_access_block" {
   bucket = aws_s3_bucket.website_bucket.id
 
-  block_public_acls   = false
-  block_public_policy = false
+  block_public_acls   = true
+  block_public_policy = true
 }
 
 resource "aws_s3_bucket_ownership_controls" "ownership" {
@@ -41,22 +41,75 @@ resource "aws_s3_bucket_policy" "bucket_policy" {
     "Version": "2012-10-17",
     "Statement": [{
       "Effect": "Allow",
-      "Principal": "*",
+      "Principal": {
+        "Service": "cloudfront.amazonaws.com"
+      },
       "Action": "s3:GetObject",
-      "Resource": "${aws_s3_bucket.website_bucket.arn}/*"
+      "Resource": "${aws_s3_bucket.website_bucket.arn}/*",
+      "Condition": {
+        "StringEquals": {
+          "AWS:SourceArn": aws_cloudfront_distribution.website_distribution.arn
+        }
+      }
     }]
   })
 }
-
 
 resource "aws_s3_object" "index_html" {
   bucket       = aws_s3_bucket.website_bucket.id
   key          = "index.html"
   source       = "index.html"
-  content_type = "text/html"  
+  content_type = "text/html"
 }
 
+resource "aws_cloudfront_origin_access_control" "s3_oac" {
+  name                             = "${var.bucket_name}-oac"
+  origin_access_control_origin_type = "s3"
+  signing_behavior                 = "always"
+  signing_protocol                 = "sigv4"
+}
+
+resource "aws_cloudfront_distribution" "website_distribution" {
+  origin {
+    domain_name = aws_s3_bucket.website_bucket.bucket_regional_domain_name
+    origin_id   = aws_s3_bucket.website_bucket.id
+
+    origin_access_control_id = aws_cloudfront_origin_access_control.s3_oac.id
+  }
+
+  enabled = true
+  default_cache_behavior {
+    target_origin_id       = aws_s3_bucket.website_bucket.id
+    viewer_protocol_policy = "redirect-to-https"
+
+    allowed_methods = ["GET", "HEAD"]
+    cached_methods  = ["GET", "HEAD"]
+
+    forwarded_values {
+      query_string = false
+      cookies {
+        forward = "none"
+      }
+    }
+
+    min_ttl     = 0
+    default_ttl = 3600
+    max_ttl     = 86400
+  }
+
+  viewer_certificate {
+    cloudfront_default_certificate = true
+  }
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
+
+  default_root_object = "index.html"
+}
 
 output "website_url" {
-  value = aws_s3_bucket.website_bucket.website_domain
+  value = aws_cloudfront_distribution.website_distribution.domain_name
 }
